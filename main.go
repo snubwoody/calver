b7c22a103f9d6d3518df8edf615aad407a422ca0
waku <wakunguma13@gmail.com>
2025-08-10 13:06:38 -0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-git/go-git/v6"
+	"github.com/go-git/go-git/v6/plumbing"
+)
+
+func main() {
+	repo, err := git.PlainOpen(".")
+	if err != nil {
+		panic(err)
+	}
+
+	tags, err := repo.Tags()
+	if err != nil {
+		panic(err)
+	}
+
+	err = tags.ForEach(func(tag *plumbing.Reference) error {
+		fmt.Printf("Tag: %v", tag)
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+}
